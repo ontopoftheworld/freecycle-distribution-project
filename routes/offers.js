d4f32d91d88ecc66ad4e791c0b742ae1ebbc1e8a
Xiuzhen Lei <xiuzhen_lei@brown.edu>
2018-04-22 17:08:18 -0400

    Message from admins alerting response to offer + connecting both users added

--- routes/offers.js
+++ routes/offers.js
@@ -208,6 +208,8 @@ router.post("/offers/:id/response", isLoggedIn, function(req, res) {
                                     user.save();
                                     foundOffer.offerResponse.push({responder: responder, responseID: newOR._id});
                                     foundOffer.save();
+				    createNewMessageWithOffer(req, foundOffer.author.id, foundOffer.author.username,
+							      offerId, foundOffer, hours, message);
                                     req.flash("success", "Your response has been sent");
                                     res.redirect("/offers");
                                 }
@@ -219,6 +221,126 @@ router.post("/offers/:id/response", isLoggedIn, function(req, res) {
         }
     });
 });
+
+var Messages = require("../models/messages");
+
+function createNewMessageWithOffer(req, toUserId, toUserName, offerId, foundOffer, hours, message) {
+    Messages.find({"$or" : [{"$and": [{"senderA.id" : req.user._id},
+				      {"senderB.id" : toUserId}]},
+			    {"$and": [{"senderA.id" : toUserId},
+				      {"senderB.id" : req.user._id}]}]},
+		  function (err, foundChat) {
+		      if (foundChat !== undefined && foundChat.length !== 0) {
+			  // previous conversation exists
+			  let cGrp = foundChat[0].chatGroup;
+			  let toUser = foundChat[0].senderA.displayName;
+			  if (toUser === req.user.firstName) {
+			      toUser = foundChat[0].senderB.displayName;
+			  }
+
+			  // change the current user's seen status to true, so that the message
+			  // can be marked as read, and the other user's seen status as false
+			  if (foundChat[0].senderA.displayName === req.user.firstName) {
+			      Messages.update(
+				  { "chatGroup" : cGrp },
+				  { $set : { "senderA.seenMessages" : true, "senderB.seenMessages" : false }},
+				  { upsert : false, multi : true },
+				  function (err, object) {
+				      if (err) {
+					  console.log("ERROR: problems updating seen status");
+				      } 
+				  }
+			      );
+			  } else {
+			      Messages.update(
+				  { "chatGroup" : cGrp },
+				  { $set : { "senderB.seenMessages" : true, "senderA.seenMessages" : false }},
+				  { upsert : false, multi : true },
+				  function (err, object) {
+				      if (err) {
+					  console.log("ERROR: problems updating seen status");
+				      } 
+				  }
+			      );
+			  }
+			  
+			  // add the message to the database
+			  let date = new Date(); 
+			  let timeU = (date.getMonth()+1) + "/" +
+			      date.getDate()  + "/" + date.getFullYear() + ", " +
+			      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+			  Messages.update(
+			      { "chatGroup" : cGrp },
+			      { $push : { "message" : { "sentBy" : "The Freecycle Distribution Project Admins",
+							"time" : timeU,
+							"info" : toUserName + ", you have a new response " +
+							"to your offer (" + foundOffer.title +
+							") from " +
+							req.user.firstName + "! " +
+							"They are offering " + hours + " hours in exchange " +
+							"for its completion." +
+							' Here is their message: "' +  message +
+						      	'". Feel free to use this chat to further discuss this with ' +
+							req.user.firstName + ". You may accept this offer from the " +
+							'"Responses to Your Offer" page or at this link: ' +
+							"/offers/" + offerId + "/response"}}},
+			      { upsert : false, multi : true },
+			      function (err, object) {
+				  if (err) {
+				      console.log("ERROR: problems updating message");
+				  } 
+			      }
+			  );
+		      } else {
+			  // create new chatgroup
+			  var cGrp = require('crypto').createHash('md5')
+			      .update(req.user._id + "" + toUserId).digest("hex");
+			  var newChat = {message: [],
+					 chatGroup: cGrp,
+					 senderA: { id : req.user._id,
+						    displayName : req.user.firstName,
+						    seenMessages : true},
+					 senderB: { id : toUserId,
+						    displayName : toUserName,
+						    seenMessages : false}};
+			  Messages.create(newChat, function(err, newM) {
+			      if(err){
+				  console.log(err);
+			      } else {
+				  // add the message to the database
+				  let date = new Date(); 
+				  let timeU = (date.getMonth()+1) + "/" +
+				      date.getDate()  + "/" + date.getFullYear() + ", " +
+				      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+				  Messages.update(
+				      { "chatGroup" : cGrp },
+				      { $push : { "message" : { "sentBy" : "The Freecycle Distribution Project Admins",
+								"time" : timeU,
+								"info" : toUserName + ", you have a new response " +
+								"to your offer (" + foundOffer.title +
+								") from " +
+								req.user.firstName + "! " +
+								"They are offering " + hours + " hours in exchange " +
+								"for its completion." +
+								' Here is their message: "' +  message +
+						      		'". Feel free to use this chat to further discuss this with ' +
+								req.user.firstName + ". You may accept this offer from the " +
+								'"Responses to Your Offer" page or at this link: ' +
+								"/offers/" + offerId + "/response"}}},
+				      { upsert : false, multi : true },
+				      function (err, object) {
+					  if (err) {
+					      console.log("ERROR: problems updating message");
+					  } 
+				      }
+				  );
+			      }
+			  });
+		      }
+		  });
+}
 
 router.get("/response", isLoggedIn, function(req, res) {
     OfferResponse.find({"responder": req.user._id}, function(err, foundResponses){
