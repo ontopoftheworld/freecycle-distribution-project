f6b1bc7eab1b39c8d4a4938eaf5c371dc5a12a87
catherine058 <37159026+catherine058@users.noreply.github.com>
2018-04-30 23:50:56 -0400

    editprofile

--- routes/account.js
+++ routes/account.js
@@ -17,6 +17,28 @@ var Offer = require("../models/offers"),
         });
     });
 
+	
+	router.get("/account/:id/edit", isLoggedIn, function(req, res) {
+		User.findById(req.user._id, function(err, user) {
+			if(err) {
+				console.log(err);
+			} else {
+     
+                res.render("editaccount", {user:user});
+            
+			}
+		});
+	});
+	router.put("/account", isLoggedIn, function(req, res) {
+    User.findByIdAndUpdate(req.user._id, req.body.account, function(err, updatedAccount) {
+        if(err){
+            res.redirect("/account");
+        } else {
+			req.flash("success","You have successfully changed your profile")
+            res.redirect("/account");
+        }
+    });
+});
     function isLoggedIn(req, res, next) {
         if(req.isAuthenticated()) {
             return next();
